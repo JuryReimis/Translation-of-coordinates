bf9018025bed632c87075df2c53f475ac9d9dd7a
JuryReimiS <indlinyury@gmail.com>
2021-05-11 11:40:46 +0300

    Добавлен Конверс для декартовых координат в сферические

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from math import sin, cos
+from math import sin, cos, sqrt, atan
 
 
 class Point:
-    def __init__(self, x, y, z, flag):         #flag показывает, в какой системе дана точка
+    def __init__(self, x, y, z, flag):  # flag показывает, в какой системе дана точка
         if flag == "cartesian":
             self.x = x
             self.y = y
@@ -13,7 +13,14 @@ class Point:
             self.azimuth = z
         else:
             print("Система координат не распознана\nПроверьте правильность ввода")
+            self.error = True
+        if not self.error:
+            self.converse(flag)
 
-    def converse(self):
-
-
+    def converse(self, flag):
+        if flag == "cartesian":
+            self.radius = sqrt(self.x**2+self.y**2+self.z**2)
+            self.polar = atan(sqrt(self.x**2+self.y**2)/self.z)
+            self.azimuth = atan(self.y/self.x)
+        elif flag == "spherical":
+            pass
