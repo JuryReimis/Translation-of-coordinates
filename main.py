21f90389d0b6df06e981995d160068c59e74c946
JuryReimiS <indlinyury@gmail.com>
2021-05-11 18:13:38 +0300

    Программа рабочая

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-from math import sin, cos, sqrt, atan, radians
+from math import sin, cos, sqrt, atan, acos, radians
 
 
 class Point:
-    def __init__(self, x, y, z, flag=None):  # flag показывает, в какой системе дана точка
-        self.error = False
-        if flag == "cartesian":
+    def __init__(self, x=None, y=None, z=None):
+        self.error = False    #маркер ошибки при получении данных
+        self.write = True     #необходимо вывести координаты точки или нет
+        if x is None:
+            self.flag = input("Введите в какой системе будут представлены координаты Декартовы(d)/Сферические(s): ")
+            if self.flag.lower() == "d":
+                self.x = int(input("Введите координату X: "))
+                self.y = int(input("Введите координату Y: "))
+                self.z = int(input("Введите координату Z: "))
+                self.flag = "cartesian"
+            elif self.flag.lower() == "s":
+                self.radius = int(input("Введите расстояние до начала координат r: "))
+                self.polar = radians(int(input("Введите полярный угол θ: ")))
+                self.azimuth = radians(int(input("Введите азимутальный угол φ: ")))
+                self.flag = "spherical"
+            else:
+                print("Неправильно введена система, перезапустите программу!")
+                self.error = True
+        else:
             self.x = x
             self.y = y
             self.z = z
-        elif flag == "spherical":
-            self.radius = x
-            self.polar = y
-            self.azimuth = z
-        else:
-            print("Система координат не распознана\nПроверьте правильность ввода")
-            self.error = True
+            self.flag = "cartesian"
+            self.write = False
         if not self.error:
-            self.converse(flag)
+            self.converse(self.flag)
+            if self.write:
+                print(
+                    f"Заданная точка в системах координат: Декартова: {(self.x, self.y, self.z)} Сферическая: {(self.radius, self.polar, self.azimuth)}")
 
     def converse(self, f):
         if f == "cartesian":
-            self.radius = sqrt(self.x**2+self.y**2+self.z**2)
-            self.polar = atan(sqrt(self.x**2+self.y**2)/self.z)
-            self.azimuth = atan(self.y/self.x)
+            self.radius = sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+            if self.z != 0:
+                self.polar = atan(sqrt(self.x ** 2 + self.y ** 2) / self.z)
+            else:
+                self.polar = acos(0)
+            self.azimuth = atan(self.y / self.x)
         elif f == "spherical":
             self.x = self.radius * sin(self.polar) * cos(self.azimuth)
             self.y = self.radius * sin(self.polar) * sin(self.azimuth)
@@ -31,10 +48,11 @@ class Point:
 
 class Distance:
     def __init__(self, point_1: Point, point_2: Point):
-        self.__distance = sqrt((point_1.x-point_2.x)**2+(point_1.y-point_2.y)**2+(point_1.z-point_2.z)**2)
+        self.__distance = sqrt(
+            (point_1.x - point_2.x) ** 2 + (point_1.y - point_2.y) ** 2 + (point_1.z - point_2.z) ** 2)
 
     def distance(self):
-        print(f"расстояние между точками с введенными координатами равно {self.__distance}")
+        print(f"Расстояние между точками с введенными координатами равно {self.__distance}")
 
 
 class Slant:
@@ -42,28 +60,21 @@ class Slant:
         self.__x = point_2.x - point_1.x
         self.__y = point_2.y - point_1.y
         self.__z = point_2.z - point_1.z
-        self.point = Point(x=self.__x, y=self.__y, z=self.__z, flag="cartesian")
-        self.angle = self.point.polar
+        self.slant_point = Point(self.__x, self.__y, self.__z)
+        self.angle = self.slant_point.polar
 
 
 if __name__ == "__main__":
-    flag = input("Введите в какой системе будут представлены координаты Декартовы(d)/Сферические(s)")
-    error = False
-    if flag.lower() == "d":
-        first = int(input("Введите координату X: "))
-        second = int(input("Введите координату Y: "))
-        third = int(input("Введите координату Z: "))
-        flag = "cartesian"
-    elif flag.lower() == "s":
-        first = int(input("Введите расстояние до начала координат r: "))
-        second = int(input("Введите полярный угол θ: "))
-        third = int(input("Введите азимутальный угол φ: "))
-        flag = "spherical"
-    else:
-        print("Неправильно введена система, перезапустите программу!")
-        error = True
-    if not error:
-        point = Point(first, radians(second), radians(third), flag)
-        print(f"Заданная точка в системах координат: Декартова: {(point.x, point.y, point.z)} Сферическая: {(point.radius, point.polar, point.azimuth)}")
-
-
+    point_1 = Point()
+    state = input("Вы хотите ввести координату второй точки? (y/n): ")
+    if state == "y":
+        point_2 = Point()
+        state = input("Вы хотите посчитать расстояние между точками? (y/n): ")
+        if state == "y":
+            dist = Distance(point_1, point_2)
+            dist.distance()
+        state = input("Вы хотите посчитать угол наклона отрезка Point_1-Point_2? (y/n): ")
+        if state == "y":
+            slant = Slant(point_1, point_2)
+            print("Угол наклона заданного отрезка равен: ", slant.angle)
+    print("Программа завершена. Спасибо!")
